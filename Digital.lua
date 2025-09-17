03c6b0d0574c71fdc08ca740c88a2c2b9201585d
Flashtynooo <157314861+Flashtynooo@users.noreply.github.com>
2025-09-17 23:07:22 +0200

    Add files via upload

---
+++ Digital.lua
@@ -0,0 +1,65 @@
+-- Script complet de la musique "Digital Circus" pour Roblox Piano
+-- Accord = wait(0.5), Note seule = wait(0.4)
+
+local args = {"play", 8}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+local args = {"play", o}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.5)
+
+local args = {"play", u}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.4)
+
+local args = {"play", t}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.4)
+
+local args = {"play", %}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+local args = {"play", y}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.5)
+
+local args = {"play", 6}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+local args = {"play", u}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.5)
+
+local args = {"play", W}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+local args = {"play", 0}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+local args = {"play", a}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.5)
+
+local args = {"play", e}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+local args = {"play", p}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.5)
+
+local args = {"play", 6}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+local args = {"play", i}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.5)
+
+local args = {"play", q}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.4)
+
+local args = {"play", s}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.4)
+
+local args = {"play", p}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.4)
+
+local args = {"play", i}
+workspace:WaitForChild("GlobalPianoConnector"):FireServer(unpack(args))
+wait(0.4)
+
